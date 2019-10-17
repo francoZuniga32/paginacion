9c985f12c3d36a8c37968230e1c52cf966fe8bd9
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2019-10-17 20:27:42 -0300

    d1710h2027

--- muestra.php
+++ muestra.php
@@ -3,22 +3,35 @@ require_once("coneccion.php");
 require_once("consulta.php");
 
 $pagina = isset($_POST['pagina']) ? (int)$_POST['pagina']: 1; //la pagina actual si es cualquier cosa es la primera sino la que sigue
-$registosPorPagina = $_POST['cantidad']; //cantidad de registros por pagina
+$registosPorPagina = 18; //cantidad de registros por pagina
 $inicio = ($pagina>0)?(($pagina * $registosPorPagina)- $registosPorPagina): 0;//la pagina inicio para la divicion de del paginador
 
 $sql = "SELECT * FROM materia LIMIT $inicio, $registosPorPagina";
 $consulta = new CONSULTA();
 $filas = $consulta->getConsulta("SELECT * FROM materia LIMIT $inicio, $registosPorPagina");
 $total = $consulta->getConsulta("SELECT * FROM materia ");
-$contador = count($total);
-$paginas = $contador/$registosPorPagina;
 
-echo $contador;
+$i = 0;
+
 foreach($filas as $fila){
-    echo "<p>".$fila['nombre']."</p>";
+    if($i % 3 == 0){
+        echo "
+        </div>
+        <div class=\"row\">";
+    }
+    echo "<div class=\"col rounded border\">".$fila['nombre']."</div>";
+    $i = $i + 1;
 }
+echo "</div></br>";
 
-echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
+paginador($total, $registosPorPagina, $pagina);
+
+function paginador($total, $registosPorPagina, $pagina){
+    $contador = count($total);
+    $paginas = intval($contador/$registosPorPagina);
+
+
+    echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
           <ul class=\"pagination\">";
             //para mostrar el prev
     		if ($pagina == 1) {
@@ -28,7 +41,7 @@ echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
     			$numero = $pagina - 1;
                 echo "
                 <li class=\"page-item\">
-                    <a class=\"page-link\" href=\"#\" aria-label=\"Previous\" onclick=\"buscar(".$numero.",".$registosPorPagina.")\">
+                    <a class=\"page-link\" href=\"#\" aria-label=\"Previous\" onclick=\"cargar(".$numero.",".$registosPorPagina.")\">
                         <span aria-hidden=\"true\">&laquo;</span>
                     </a>
                 </li>";
@@ -39,9 +52,9 @@ echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
             if ($pagina < 3) {
                 for($i = 1; $i <= 3; $i++){
                     if ($i == $pagina) {
-                        echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                        echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                     }else{
-                        echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                        echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                     }
                 }
                 echo "<li class=\"page-item\"><a class=\"page-link\" href=\"#\" role=\"button\">...</a></li>";
@@ -51,17 +64,17 @@ echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
 
                     for($i = $paginas - 2 ; $i<= $paginas; $i++){
                         if ($i == $pagina) {
-                            echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                            echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                         }else{
-                            echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                            echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                         }                    }
                 }else{
                     echo "<li class=\"page-item\"><a class=\"page-link\" href=\"#\" role=\"button\">...</a></li>";
                     for($i = $pagina - 1 ; $i <= $pagina + 1; $i++){
                         if ($i == $pagina) {
-                            echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                            echo "<li class=\"page-item active\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                         }else{
-                            echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"buscar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
+                            echo "<li class=\"page-item\" id=\"".$i."\"><a class=\"page-link\" href=\"#\" role=\"button\" onclick=\"cargar(".$i.",".$registosPorPagina.")\">".$i."</span></a></li>";
                         }                    }
                     echo "<li class=\"page-item\"><a class=\"page-link\" href=\"#\" role=\"button\">...</a></li>";
                 }
@@ -74,11 +87,12 @@ echo "<nav class=\"Page navigation example d-flex justify-content-center\" >
     			$numero = $pagina + 1;
                 echo "
                 <li class=\"page-item\">
-                    <a class=\"page-link\" href=\"#\" aria-label=\"Previous\" onclick=\"buscar(".$numero.",".$registosPorPagina.")\">
+                    <a class=\"page-link\" href=\"#\" aria-label=\"Previous\" onclick=\"cargar(".$numero.",".$registosPorPagina.")\">
                         <span aria-hidden=\"true\">&raquo;</span>
                     </a>
                 </li>";
     		}
         	echo "</ul>
         </nav>";
+}
 ?>
